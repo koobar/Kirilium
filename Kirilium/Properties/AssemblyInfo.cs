bff84dbdd8722bac6e22665d8e1bf932cbb22dd7
koobar <koobar14@gmail.com>
2024-07-03 11:37:35 +0900

    アセンブリ情報の設定のうち、タイトルや製品名の項目がコードネームのままになっていたので修正。

--- Kirilium/Properties/AssemblyInfo.cs
+++ Kirilium/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("Koobar.Widgets")]
+[assembly: AssemblyTitle("Kirilium")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Koobar.Widgets")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyProduct("Kirilium")]
+[assembly: AssemblyCopyright("Copyright (c) 2024 koobar.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
